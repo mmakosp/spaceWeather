896c48bf0c70aed2ed10c77c68897d6b82d050bf
Mmako Sekhosana <mmako@glucode.com>
2023-06-12 21:09:54 +0200

    Initial Commit

---
+++ spaceWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spaceWeather
+//
+//  Created by Mmako Sekhosana on 2023/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
